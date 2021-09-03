07ed950a5b899f4c2a1ea2a1bb6c20c2b6e79e11
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2021-09-04 01:13:28 +0200

    Added HEX Input Field
    Without validator yet.

--- lib/color_picker/ui/view/colorpicker_dialog.dart
+++ lib/color_picker/ui/view/colorpicker_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
@@ -11,6 +12,7 @@ import '../../../core/extensions/color.dart';
 import '../../../favorites/blocs/add_favorites/fab_bloc.dart';
 import '../../../sound/blocs/sounds_vibration/sound_bloc.dart';
 import '../../blocs/colorpicker_dialog/colorpicker_bloc.dart';
+import '../../helpers/hex_formatter.dart';
 
 class Colorpicker extends StatelessWidget {
   const Colorpicker(
@@ -32,14 +34,17 @@ class Colorpicker extends StatelessWidget {
         child: BlocBuilder<ColorPickerBLoc, ColorPickerState>(
           builder: (dialogContext, state) {
             if (state is ColorPickerOpenInitial) {
-              SchedulerBinding.instance?.addPostFrameCallback((_) async => showModal<void>(
-                    context: dialogContext,
-                    configuration: const FadeScaleTransitionConfiguration(
-                      barrierColor: Colors.transparent,
-                      transitionDuration: Duration(milliseconds: 400),
-                      reverseTransitionDuration: Duration(milliseconds: 200),
-                    ),
-                    builder: (_) => SimpleDialog(
+              SchedulerBinding.instance?.addPostFrameCallback(
+                (_) async => showModal<void>(
+                  context: dialogContext,
+                  configuration: const FadeScaleTransitionConfiguration(
+                    barrierColor: Colors.transparent,
+                    transitionDuration: Duration(milliseconds: 400),
+                    reverseTransitionDuration: Duration(milliseconds: 200),
+                  ),
+                  builder: (_) {
+                    final TextEditingController hexController = TextEditingController();
+                    return SimpleDialog(
                       contentPadding: const EdgeInsets.all(0),
                       children: [
                         ColorPicker(
@@ -47,15 +52,49 @@ class Colorpicker extends StatelessWidget {
                           enableAlpha: false,
                           portraitOnly: true,
                           displayThumbColor: true,
+                          showLabel: false,
                           pickerColor: color,
+                          hexInputController: hexController,
                           onColorChanged: (newColor) {
                             BlocProvider.of<FabBloc>(context).add(const FabShowed());
-                            BlocProvider.of<ColorsBloc>(context).add(ColorsChanged(newColor, index));
+                            BlocProvider.of<ColorsBloc>(context)
+                                .add(ColorsChanged(newColor, index));
                           },
                         ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(24, 0, 24, 24),
+                          child: TextFormField(
+                            controller: hexController,
+                            autofocus: true,
+                            maxLength: 6,
+                            //TODO: Add validator.
+                            autocorrect: false,
+                            textAlignVertical: TextAlignVertical.center,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(Icons.tag),
+                              suffixIcon: IconButton(
+                                tooltip: MaterialLocalizations.of(context).pasteButtonLabel,
+                                icon: Icon(
+                                  Icons.content_paste_outlined,
+                                  color: Theme.of(context).textTheme.bodyText1?.color,
+                                ),
+                                onPressed: () async {
+                                  final text = HexFormatter.formatCompleteInput(
+                                      (await Clipboard.getData('text/plain'))?.text);
+                                  if (text != null) {
+                                    hexController.text = text;
+                                  }
+                                },
+                              ),
+                            ),
+                            inputFormatters: [HexFormatter()],
+                          ),
+                        ),
                       ],
-                    ),
-                  ));
+                    );
+                  },
+                ),
+              );
               BlocProvider.of<ColorPickerBLoc>(dialogContext).add(const ColorPickerHided());
             }
             return TextButton(
@@ -84,11 +123,15 @@ class Colorpicker extends StatelessWidget {
                       onTap: isPortrait
                           ? null
                           : () {
-                              BlocProvider.of<ColorPickerBLoc>(context).add(ColorPickerCopied(color));
-                              BlocProvider.of<SnackbarBloc>(context).add(const ColorCopiedSuccess());
+                              BlocProvider.of<ColorPickerBLoc>(context)
+                                  .add(ColorPickerCopied(color));
+                              BlocProvider.of<SnackbarBloc>(context)
+                                  .add(const ColorCopiedSuccess());
                             },
                       child: Text(color.toHex(),
-                          maxLines: 1, textAlign: TextAlign.center, style: TextStyle(color: textColor)),
+                          maxLines: 1,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: textColor)),
                     ),
                   ),
                 ),
