36fd4176b0d96da41c02c0bde646440b79cdf1c6
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2022-09-08 23:58:39 +0200

    feat: generate asset resources

---
+++ lib/resources/fonts.dart
@@ -0,0 +1,6 @@
+class Fonts {
+  static const String robotoRegular = 'assets/google_fonts/Roboto-Regular.ttf';
+  static const String robotoMonoRegular = 'assets/google_fonts/RobotoMono-Regular.ttf';
+
+  const Fonts._();
+}
