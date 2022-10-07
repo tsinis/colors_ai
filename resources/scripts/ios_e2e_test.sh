bea330554dbfa481de4c5c1a2dd9da55d6418bbc
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2022-10-07 15:55:03 +0200

    fix(codemagic): remove artifacts from codemagic e2e

--- resources/scripts/ios_e2e_test.sh
+++
@@ -1,5 +0,0 @@
-open -a Simulator
-mkdir -p test-results
-flutter devices
-flutter test integration_test/onboarding_test.dart --machine > test-results/integration-ios.json
-zip -r test_results.zip test-results/
