3a27e2e65e9b44fd5bb1d0e7418c2478e342165e
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2023-05-30 22:15:00 +0200

    fix(test): fix tests for sounds player

--- lib/sound/services/sounds_player.dart
+++ lib/sound/services/sounds_player.dart
@@ -1,7 +1,9 @@
 import 'package:audioplayers/audioplayers.dart';
 
 class SoundsPlayer {
-  final AudioPlayer _audioPlayer = AudioPlayer();
+  final AudioPlayer _audioPlayer;
+
+  SoundsPlayer([AudioPlayer? audioPlayer]) : _audioPlayer = audioPlayer ?? AudioPlayer();
 
   // ignore: unnecessary-nullable, it's easier to test nullable arguments (for stubs).
   Future<void> playSound(String asset, double? volume, {List<String>? cachedFileNames}) async {
@@ -9,11 +11,11 @@ class SoundsPlayer {
     if (cacheOnly && volume != null) {
       final double normalizedVolume = volume.clamp(0, 1);
       await _audioPlayer.audioCache.loadAll(cachedFileNames);
-      await _audioPlayer.setVolume(normalizedVolume);
+
+      return _audioPlayer.setVolume(normalizedVolume);
     } else if (!cacheOnly) {
-      final AssetSource sound = AssetSource(asset);
       // TODO: Turn on  mode: PlayerMode.lowLatency after fix.
-      await _audioPlayer.play(sound, volume: volume);
+      return _audioPlayer.play(AssetSource(asset), volume: volume);
     }
   }
 }
